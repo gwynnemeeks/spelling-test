5589788bedc4b68af9fa9ff60a5b061c5b2a64ff
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-11 18:18:54 -0600

    wip

---
+++ src/components/Score.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <p>score</p>
+</template>
+
+<script>
+export default {
+    props: ["questions"],
+    computed: {
+        correctCount() {
+            return this.questions.filter(q => {
+                return q.word.toLowerCase() === q.userInput.toLowerCase();
+            }).length;
+        },
+    }
+};
+</script>
+
+<style>
+
+</style>
